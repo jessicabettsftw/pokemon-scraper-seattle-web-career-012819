ff1967ddf27dad1a72c0a8d2a5d12f0f03cfa31f
github username <github email address>
2019-02-07 10:00:25 -0800

    pokemon base class

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,21 @@
 class Pokemon
+  attr_reader :id, :name, :type, :db
+
+  @@all =[]
+
+  def initialize(id: , name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.save()
+
+  end
 end
