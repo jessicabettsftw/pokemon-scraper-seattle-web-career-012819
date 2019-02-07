d9194d65a0819cacd6ca85a25edbeab72ca8cd27
github username <github email address>
2019-02-07 13:02:48 -0800

    done

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,7 +15,12 @@ class Pokemon
     @@all
   end
 
-  def self.save()
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon ('name', 'type') VALUES ('#{name}','#{type}');")
+  end
 
+  def self.find(id, db)
+    results = db.execute("SELECT * FROM pokemon WHERE id = #{id};")
+    my_pokemon = Pokemon.new(id: results[0][0],name: results[0][1], type: results[0][2],db:  db)
   end
 end
